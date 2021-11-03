f314ee527dec32b2a28c94609bfbc25d4a672c89
Onuralp <xonuraralpx@gmail.com>
2021-11-03 16:22:01 +0300

    Initial Commit

---
+++ ParseInstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParseInstagramClone
+//
+//  Created by Ceren Çapar on 3.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
